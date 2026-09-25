cfd45d48a23dd63ef0168cd07f8325e815c6bdde
doananhkien3007-pixel <doananhkien3007@gmail.com>
2026-09-25 15:00:41 +0700

    s

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -7,7 +7,7 @@ export async function GET(req: Request) {
     const token = searchParams.get('hub.verify_token');
     const challenge = searchParams.get('hub.challenge');
 
-    const VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
+    const VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN || 'anhkien123';
 
     if (mode && token) {
       if (mode === 'subscribe' && token === VERIFY_TOKEN) {
